ec0035afee14600513d66f2ad001982c8312bd2f
Mariusz <mariuszbia@yahoo.com>
2022-04-06 21:04:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    const yearRecord = record.find(isWinningYear);
+    return yearRecord === undefined? undefined: yearRecord.year;
+}
+
+function isWinningYear(element) {
+    return element.result === `W`;
+}
